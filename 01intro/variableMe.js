86ea07f7c4456a18a7c26e6b6cd3edb5b6cbd0a0
rijalsujan09 <srijal@castera.io>
2024-01-26 15:44:17 -0500

    type script intro

---
+++ 01intro/variableMe.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+// let variableName : type = value ;
+var greetings = "Hello Sujan";
+greetings.toLowerCase();
+console.log(greetings);
+// number
+var userId = 334455.3;
+// boolean
+var isLoggedIn = false;
---
+++ 01intro/variableMe.ts
@@ -0,0 +1,18 @@
+// let variableName : type = value ;
+let greetings : string  = "Hello Sujan";
+
+greetings.toLowerCase();
+console.log(greetings);
+
+// number
+
+let userId: number = 334455.3;
+
+// boolean
+let isLoggedIn : boolean = false;
+
+
+
+
+
+export {}
