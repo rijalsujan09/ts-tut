6cf01b30ba55a3a021ca2c89dd5e006aaf99cf4b
rijalsujan09 <srijal@castera.io>
2024-01-26 16:28:58 -0500

    arrays

---
+++ 03arrays/myArray.js
@@ -0,0 +1,11 @@
+var superHeros = [];
+// const heroPower: number[] = [];
+var heroPower = [];
+var allUser = [];
+var MLModels = [
+    [25, 25, 25]
+    [36, 36, 36]
+];
+superHeros.push('spiderman');
+heroPower.push(2);
+allUser.push({ name: 'sujan', isActive: true });
---
+++ 03arrays/myArray.ts
@@ -0,0 +1,24 @@
+const superHeros : string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = { 
+    name : string, 
+    isActive: boolean,
+}
+
+const allUser : User[] = [];
+
+const MLModels: number[][] =[
+    [25, 25, 25], 
+    [36, 36, 36]
+]
+
+
+
+superHeros.push('spiderman')
+heroPower.push(2);
+
+
+
+allUser.push({name:'sujan', isActive:true})
